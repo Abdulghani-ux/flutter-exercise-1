94aaca422b643e2f5a5b3689d8debb627c59f13f
nasir4440 <abdulghanihusein@gmail.com>
2022-10-04 13:24:07 +0300

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ void main() {
                               color: Colors.yellow,
                               borderRadius: BorderRadius.all(Radius.circular(30.0))
                           ),
-                          child: Icon(Icons.share,color: Colors.blue),
+                          child: Icon(Icons.share,color: Colors.blue,
+                          size: 40,),
                         ),
                         Text('share'),
                       ],
@@ -30,7 +31,9 @@ void main() {
                     Column(
                       children: [
                         Container(
-                          child: Icon(Icons.shop,color: Colors.red),
+                          child: Icon(Icons.shop,color: Colors.red,
+                            size: 40,
+                          ),
                         ),
                         Text("Shop")
                       ],
@@ -38,9 +41,10 @@ void main() {
                     Column(
                       children: [
                         Container(
-                          child: Icon(Icons.email,color: Colors.green),
+                          child: Icon(Icons.email,color: Colors.green,
+                          size: 40,),
                         ),
-                        Text("Share")
+                        Text("email")
                       ],
                     ),
                   ],
